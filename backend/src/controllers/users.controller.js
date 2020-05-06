5b1014100a5455acfe8bba37d45402b937c51fcf
najerarodriguez <najerarodriguez04@gmail.com>
2020-05-05 18:49:29 -0600

    dos

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -1,30 +1,36 @@
-const userCtrl={};
-const User= require('../model/User');
+const userCtrl = {};
+
+const User = require('../models/User');
+
 userCtrl.getUsers = async (req, res) => {
-	try {
-		const users=await User.find(); /*Buscar usuario*/
-		res.json(users);
-	}
-	catch (err) {
-		res.status(400).json({ 
-			error: err
-		}); 
-	}
+    try {
+        const users = await User.find();
+        res.json(users);
+    }
+    catch (err) {
+        res.status(400).json({
+            error: err
+        });
+    }
 };
-userCtrl.createUser=async (req,res)=>{
-	try { /*Crear un usuario*/
-		const { username }= req.body;
-		const newUser = new User({username});
-		await newUser.save();
-		res.json('User Created');
-	}
-	catch (e){ /*Anuncio de error*/
-		console.long(e)
-		res.json(e.errmsg);
-	}
+
+userCtrl.createUser = async (req, res) => {
+    try {
+        const { username } = req.body;
+
+        const newUser = new User({ username });
+        await newUser.save();
+        res.json('User created');
+    } catch (e) {
+        console.log(e)
+        res.json(e.errmsg);
+    }
 };
-userCtrl.deleteUser=async(req,res)=>{
-	const { id }= req.params;
-	await User.findByIdAndDelete(id);
-	res.json('Deleted');
+
+userCtrl.deleteUser = async (req, res) => {
+    const { id } = req.params;
+    await User.findByIdAndDelete(id);
+    res.json('User deleted');
 }
+
+module.exports = userCtrl;
--- backend/src/database.js
+++ backend/src/database.js
@@ -1,13 +1,18 @@
-/*Hacer conexion de base de datos*/
+
 
 const mongoose = require('mongoose');
-const URI = process.env.mongodburi;/*direccion donde va a estar la base de datos*/
-mongoose.connect(URI,
-{ useNewUrlParser:true,
-	useCreateIndex:true
-}
-	);
+
+const URI = process.env.MONGOOSE_URI
+    ? process.env.MONGOOSE_URI
+    : 'mongodb://localhost/merndatabase';
+
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
 const connection = mongoose.connection;
+
 connection.once('open', () => {
-	console.log('dbinconnected');
+    console.log('Database is connected');
 });
